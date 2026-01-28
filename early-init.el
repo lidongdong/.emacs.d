8bf96daeb7f173f5e705ec94b043cb9ccc4245f8
dli <dong.li@yingren.cn>
2026-01-28 16:34:32 +0800

    config: 1) turn off menu-bar 2) turn off help-quick buffer

--- early-init.el
+++ early-init.el
@@ -27,6 +27,7 @@
 
 ;; Default frame configuration: full screen, good-looking title bar on macOS
 (setq frame-resize-pixelwise t)
+(menu-bar-mode -1)
 (tool-bar-mode -1)                      ; All these tools are in the menu-bar anyway
 (setq default-frame-alist '((fullscreen . maximized)
 
